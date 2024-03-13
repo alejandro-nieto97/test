25e8207426d8ab14b0dabe5be1d2244367c6a5ce
alejandro <alejandro.nieto@bandofcoders.com>
2024-03-13 00:26:36 -0700

    update next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/test-frontend',
+  assetPrefix: '/test-frontend',
+};
 
 export default nextConfig;
